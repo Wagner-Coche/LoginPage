a68f5d0070cc027c96f0ab5b4ad3fb33c555d690
Wagner-Coche <wagnercoche@gmail.com>
2021-12-31 22:12:41 +0100

    updated file buttonlogin

---
+++ lib/buttonlogin.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class ButtonLogin extends StatelessWidget{
+  
+  @override
+  Widget build(BuildContext context) {
+    
+    return SizedBox(
+      child: ElevatedButton(
+        onPressed: (){
+          
+        },
+        child: SizedBox(
+          child: Text(
+            "Login",
+          ),
+        ),
+      ),
+    );
+  }
+ 
+}
