13565640fc09ac9c60d0b837223dfc284333cbb1
Wagner-Coche <wagnercoche@gmail.com>
2021-12-31 12:31:29 +0100

    file app updated

---
+++ lib/app.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class App extends StatelessWidget{
+  
+  @override
+  Widget build(BuildContext context) {
+    
+    return SizedBox(
+      child: MaterialApp(
+        theme: ThemeData(
+          primarySwatch: Colors.purple,
+        ),
+        home: Container(
+          child: LoginPage(),
+        ),
+      ),
+    );
+  }
+ 
+}
